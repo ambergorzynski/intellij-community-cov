477a63114413b5e5952260d89901c471326fea4c
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-05-07 21:05:46 +0100

    Add jar filepath

--- plugins/java-decompiler/engine/test/org/jetbrains/java/decompiler/FuzzingTestImplementation.java
+++ plugins/java-decompiler/engine/test/org/jetbrains/java/decompiler/FuzzingTestImplementation.java
@@ -85,7 +85,13 @@ class FuzzingTestImplementation {
                 Element clazz = (Element)classes.item(x);
                 String classFilePath = clazz.getElementsByTagName("path").item(0).getTextContent();
                 String name = clazz.getElementsByTagName("name").item(0).getTextContent();
-                Path expectedSource = Paths.get(classFilePath + "/" + name + ".class");
+                Path expectedSource;
+                if(name.contains(".jar")){
+                    expectedSource = Paths.get(classFilePath + "/" + name);
+                }
+                else {
+                    expectedSource = Paths.get(classFilePath + "/" + name + ".class");
+                }
                 res.add(Arguments.of(expectedSource));
 
             }
